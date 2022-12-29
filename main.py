62f074abcce388c529aeec758541f97210eb3825
Carlos Infante <kuva5008@gmail.com>
2022-12-29 23:59:15 +0100

    done

--- main.py
+++ main.py
@@ -41,26 +41,58 @@ class Player():
 
 
 alice = Player('Alice', 0.8, 0.2, 0.6)
-bob = Player('Bob', 0.9, 0.2, 0.6)
+bob = Player('Bob', 0.7, 0.2, 0.6)
 
 print(alice)
 print(bob)
-print('Alice', alice.strength())
-print('Bob', bob.strength())
+# print('Alice', alice.strength())
+# print('Bob', bob.strength())
+
+
 # PART 2
 
-'''
-class Commentator(Player):
+
+class Commentator():
+    profession = 'Comentator'
+
     def __init__(self, name):
         self.name = name
 
-    def sum_player(Player):
+    def __str__(self):
+        return f"Hello everyone, my name is {self.name} and I'm a {self.profession}"
 
-        return Player.speed + Player.endurance + Player.accuracy
+    def sum_player(self, player):
+        self.player = player
+        speed = getattr(player, 'speed')
+        endurance = getattr(player, 'endurance')
+        accuracy = getattr(player, 'accuracy')
 
-    def compare_players(player_1, player_2, )
+        return speed + endurance + accuracy
+
+    def compare_players(self, player_1, player_2, att):
+        player_1_sum = self.sum_player(player_1)
+        player_2_sum = self.sum_player(player_2)
+        x = player_1.strength()[1]
+        y = player_2.strength()[1]
+
+        if player_1.speed > player_2.speed:
+            return player_1.name
+        elif player_1.speed < player_2.speed:
+            return player_2.name
+        else:
+            if x > y:
+                return player_1.name
+            elif x < y:
+                return player_2.name
+            else:
+                if player_1_sum > player_2_sum:
+                    return player_1.name
+                elif player_1_sum < player_2_sum:
+                    return player_2.name
+                else:
+                    return 'These two players might as well be twins!'
 
 
 ray = Commentator('Ray Hudson')
 print(ray.name)
-'''
+print(ray.compare_players(alice, bob, 'speed'))
